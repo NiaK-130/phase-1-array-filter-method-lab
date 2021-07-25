0cf3e8863c7b40ed2d173a12fb78f797f35a4993
Nia K <nsjkappen@gmail.com>
2021-07-25 13:39:11 -0700

    commit tree f5fc29fee50fb8bdf4ee971019732d7e6b6a1b2d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter(person => person.toLowerCase()=== name.toLowerCase());
+    return newDrivers;
+
+}
+
+
+const fuzzyMatch = (drivers, name) => {
+    return drivers.filter(el => el.toLowerCase().indexOf(name.toLowerCase()) !== -1)
+
+}
